259fadaf5063636f81d7d3ec17ad3bf7f90e0623
MrHurried <69963194+MrHurried@users.noreply.github.com>
2022-12-17 20:31:46 +0100

    Updated GameMode2
    -Move around a couple of testing functions from piecegenscript to notecarouselscript
    -renamed a couple of functions
    -changed some function's behaviours

--- Assets/NoteCarouselScript.cs
+++ Assets/NoteCarouselScript.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class NoteCarouselScript : MonoBehaviour
@@ -8,15 +10,125 @@ public class NoteCarouselScript : MonoBehaviour
 
     private string[] generatedPiece;
 
+    public int bpm = 60;
+    public int currentNoteIndex = 0;
+
+    [SerializeField] Transform nbHolder;
+    Transform nb0,nb1, nb2, nb3, nb4;
+
+    //NOTE SPRITES
+    [SerializeField] Sprite note2;
+    [SerializeField] Sprite note4;
+    [SerializeField] Sprite note8;
+    [SerializeField] Sprite note16;
+    [SerializeField] Sprite rest2;
+    [SerializeField] Sprite rest4;
+    [SerializeField] Sprite rest8;
+    [SerializeField] Sprite rest16;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        nb0 = nbHolder.GetChild(0);
+        nb1 = nbHolder.GetChild(1);
+        nb2 = nbHolder.GetChild(2);
+        nb3 = nbHolder.GetChild(3);
+        nb4 = nbHolder.GetChild(4);
         generatedPiece = randPieceScript.generatedPiece;
+
+        //StartCoroutine(tempAutomaticProceedCarousel());
     }
 
-    // Update is called once per frame
-    void Update()
+    //50 calls per second
+   void FixedUpdate()
     {
-        
+        int cps = 50;// calls per second
+        for (int i = 0; i < cps * 30; i++){
+            const float nbDistance = 1.1f;
+            // I want to make the blocks move the blocks one "space" (= nb2 gets nb2's position) in bpm / 60f seconds
+            float unitsToMove = nbDistance / cps;
+
+            Vector3 moveVector = new Vector3(-unitsToMove, 0f, 0f);
+            nbHolder.Translate(moveVector);
+        }
+    }
+
+    IEnumerator tempAutomaticProceedCarousel()
+    {
+
+        for (; ; )
+        {
+            int symbolLength = getNoteOrRestLength(generatedPiece[currentNoteIndex]);
+            Debug.Log("symbol length: " + symbolLength);
+
+
+            //epic gamer formula
+            float beatspersecond = 0f;
+            float temp = 0f;
+            float secondsToWait = (bpm / 60f) / (symbolLength / 4f);
+
+            Debug.Log(secondsToWait);
+
+            yield return new WaitForSeconds(secondsToWait);
+
+            if (RandomPieceGeneratorScript.noteAmount - 1 > currentNoteIndex) currentNoteIndex++;
+            else break;
+        }
+
+    }
+
+    public Sprite getCurrentNoteSprite()
+    {
+        string strCurrentNote = generatedPiece[currentNoteIndex];
+        Sprite sprite;
+
+        //Set sprite var to right image, according to the current note (strCurrentNote)
+        switch (strCurrentNote)
+        {
+            case "2":
+                sprite = note2;
+                break;
+            case "4":
+                sprite = note4;
+                break;
+            case "8":
+                sprite = note8;
+                break;
+            case "16":
+                sprite = note16;
+                break;
+            case "r2":
+                sprite = rest2;
+                break;
+            case "r4":
+                sprite = rest4;
+                break;
+            case "r8":
+                sprite = rest8;
+                break;
+            case "r16":
+                sprite = rest16;
+                break;
+            default:
+                sprite = note2;
+                break;
+        }
+
+        return sprite;
+    }
+
+
+    // Update is called once per frame
+    public int getNoteOrRestLength(string strNote)
+    {
+        if (strNote[0] == 'r')
+        {
+            return int.Parse(strNote.Replace("r", ""));
+        }
+        else
+        {
+            return int.Parse(strNote);
+        }
     }
 }
--- Assets/Scripts/GameMode2/RandomPieceGeneratorScript.cs
+++ Assets/Scripts/GameMode2/RandomPieceGeneratorScript.cs
@@ -7,24 +7,11 @@ public class RandomPieceGeneratorScript : MonoBehaviour
     // PIECE GENERATION RELATED
     public static int noteAmount = 50;
 
-    public int currentNoteIndex = 0;
-
     string[] availableNotes = { "2", "4", "8", "16", "r2", "r4", "r8", "r16" };
 
     public string[] generatedPiece = new string[noteAmount];
 
     //MISC
-    public int bpm = 60;
-
-    //NOTE SPRITES
-    [SerializeField] Sprite note2;
-    [SerializeField] Sprite note4;
-    [SerializeField] Sprite note8;
-    [SerializeField] Sprite note16;
-    [SerializeField] Sprite rest2;
-    [SerializeField] Sprite rest4;
-    [SerializeField] Sprite rest8;
-    [SerializeField] Sprite rest16;
 
     /// <summary>
     /// triple slash = summary :p
@@ -48,17 +35,12 @@ public class RandomPieceGeneratorScript : MonoBehaviour
 
         Debug.Log(testStr);
 
-        StartCoroutine(tempAutomaticProceedCarousel());
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && (noteAmount-1 > currentNoteIndex))
-        {
-            
-        }
-
         
     }
 
@@ -70,121 +52,5 @@ public class RandomPieceGeneratorScript : MonoBehaviour
             generatedPiece[i] = availableNotes[rnd];
         }
     }
-
-    void tempManualProceedCarousel()
-    {
-        currentNoteIndex++;
-        string strCurrentNote = generatedPiece[currentNoteIndex];
-        Sprite sprite;
-
-        //Set sprite var to right image, according to the current note (strCurrentNote)
-        switch (strCurrentNote)
-        {
-            case "2":
-                sprite = note2;
-                break;
-            case "4":
-                sprite = note4;
-                break;
-            case "8":
-                sprite = note8;
-                break;
-            case "16":
-                sprite = note16;
-                break;
-            case "r2":
-                sprite = rest2;
-                break;
-            case "r4":
-                sprite = rest4;
-                break;
-            case "r8":
-                sprite = rest8;
-                break;
-            case "r16":
-                sprite = rest16;
-                break;
-            default:
-                sprite = note2;
-                break;
-        }
-
-        srTest.sprite = sprite;
-    }
-
-    IEnumerator tempAutomaticProceedCarousel()
-    {
-        for(; ; )
-        { 
-            tempDisplayNote();
-            int symbolLength = getNoteOrRestLength(generatedPiece[currentNoteIndex]);
-            Debug.Log("symbol length: " + symbolLength);
-            
-            
-            //epic gamer formula
-            float beatspersecond = 0f;
-            float temp = 0f;
-            float secondsToWait = (bpm / 60f) / (symbolLength / 4f);
-
-            Debug.Log(secondsToWait);
-
-            yield return new WaitForSeconds(secondsToWait);
-
-            if (noteAmount - 1 > currentNoteIndex) currentNoteIndex++;
-            else break;
-        }
-
-    }
-
-    public void tempDisplayNote()
-    {
-        string strCurrentNote = generatedPiece[currentNoteIndex];
-        Sprite sprite;
-
-        //Set sprite var to right image, according to the current note (strCurrentNote)
-        switch (strCurrentNote)
-        {
-            case "2":
-                sprite = note2;
-                break;
-            case "4":
-                sprite = note4;
-                break;
-            case "8":
-                sprite = note8;
-                break;
-            case "16":
-                sprite = note16;
-                break;
-            case "r2":
-                sprite = rest2;
-                break;
-            case "r4":
-                sprite = rest4;
-                break;
-            case "r8":
-                sprite = rest8;
-                break;
-            case "r16":
-                sprite = rest16;
-                break;
-            default:
-                sprite = note2;
-                break;
-        }
-
-        srTest.sprite = sprite;
-    }
-
-    public int getNoteOrRestLength(string strNote)
-    {
-        if (strNote[0] == 'r')
-        {
-            return int.Parse(strNote.Replace("r", ""));
-        }
-        else
-        {
-            return int.Parse(strNote);
-        }
-    }
+    
 }
