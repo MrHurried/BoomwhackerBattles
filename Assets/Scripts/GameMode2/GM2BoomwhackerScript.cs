3ede5d1ea49b6154b3988e4329e3665959c3f04b
MrHurried <69963194+MrHurried@users.noreply.github.com>
2023-02-11 17:11:15 +0100

    Added Autosave an and BW animation

---
+++ Assets/Scripts/GameMode2/GM2BoomwhackerScript.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM2BoomwhackerScript : MonoBehaviour
+{
+    //AUDIO RELATED
+    [SerializeField] AudioClip bonk;
+    [SerializeField] AudioSource audioSrc;
+
+    //SPRITES
+    [SerializeField] Sprite BWSwingSprite;
+    [SerializeField] Sprite BWSprite;
+
+    [SerializeField] Sprite MatisseWhackedSprite;
+    [SerializeField] Sprite IsabelWhackedSprite;
+
+    [SerializeField] Sprite MatisseSprite;
+    [SerializeField] Sprite IsabelSprite;
+
+    //SPRITE RENDERERS
+    //"BoomWhacker _ Sprite Renderer _ Matisse"
+    [SerializeField] SpriteRenderer BW_SR_MAT;
+    [SerializeField] SpriteRenderer BW_SR_ISA;
+
+    [SerializeField] SpriteRenderer Matisse_SR;
+    [SerializeField] SpriteRenderer Isabel_SR;
+
+    //PRIMITIVE DATA TYPES
+    //This defines how long the swing sprite will stay visible
+    public float swingTime = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            //whack Isabel
+            StartCoroutine(whackCoroutine(false));
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            //Whack Matisse
+            StartCoroutine(whackCoroutine(true));
+        }
+    }
+
+    IEnumerator whackCoroutine(bool whackMatisse)
+    {
+        if (whackMatisse)
+        {
+            BW_SR_MAT.sprite = BWSwingSprite;
+            Matisse_SR.sprite = MatisseWhackedSprite;
+            yield return new WaitForSeconds(swingTime);
+            BW_SR_MAT.sprite = BWSprite;
+            Matisse_SR.sprite = MatisseSprite;
+        }
+        else
+        {
+            BW_SR_ISA.sprite = BWSwingSprite;
+            Isabel_SR.sprite = IsabelWhackedSprite;
+            yield return new WaitForSeconds(swingTime);
+            Isabel_SR.sprite = IsabelSprite;
+            BW_SR_ISA.sprite = BWSprite;
+        }
+    }
+}
