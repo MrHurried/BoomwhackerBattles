baa3ec80ca4025cc28b0ea4c5ed0df28f499ba42
MrHurried <69963194+MrHurried@users.noreply.github.com>
2023-02-12 12:24:00 +0100

    Fixed the black streak number text

--- Assets/Scripts/GameMode2/StreakScript.cs
+++ Assets/Scripts/GameMode2/StreakScript.cs
@@ -7,16 +7,24 @@ public class StreakScript : MonoBehaviour
 {
     public TextMeshProUGUI streakOnderIsaText;
 
+    //TESTING
+
+    [SerializeField] SpriteRenderer topTester_SR;
+    [SerializeField] SpriteRenderer bottomTester_SR;
+
+    //TESTING OVER
+
+
     private Color isaTopColor, isaBottomColor;
     private Color matTopColor, matBottomColor;
 
-    private const float S_TopColor = 82/100;
-    private const float S_BottomColor = 62/100;
-    private const float V_TopColor = 58/100;
-    private const float V_BottomColor = 91/100;
+    private const float S_TopColor = 82f/100f;
+    private const float S_BottomColor = 62f/100f;
+    private const float V_TopColor = 58f/100f;
+    private const float V_BottomColor = 91f/100f;
     //the Hue values at every reset
-    private const float H_TopDefault = 274/360;
-    private const float H_BottomDefault = 256/360;
+    private const float H_TopDefault = 274f/360f;
+    private const float H_BottomDefault = 256f/360f;
 
     //Measured in degrees (max = 360; min = 0)
     public float HueChangeMagnitude = 2/360;
@@ -45,7 +53,16 @@ public class StreakScript : MonoBehaviour
         isaTopColor = Color.HSVToRGB(H_TopDefault, S_TopColor, V_TopColor);
         isaBottomColor = Color.HSVToRGB(H_BottomDefault, S_BottomColor, V_BottomColor);
 
+        topTester_SR.color = isaTopColor;
+        bottomTester_SR.color = isaBottomColor;
+
+        Debug.Log("H_default bottom: " + H_BottomDefault);
+        Debug.Log("S_default bottom: " + S_BottomColor);
+        Debug.Log("V_default bottom: " + V_BottomColor);
+
+        streakOnderIsaText.enableVertexGradient = true;
         streakOnderIsaText.colorGradient = new VertexGradient(isaTopColor, isaTopColor, isaBottomColor, isaBottomColor);
+        streakOnderIsaText.enableVertexGradient = true;
     }
 
     void shiftHue(bool isMatisse)
