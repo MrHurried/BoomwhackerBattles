6e348787e035f41b2800f710bc9b7b4dbdba544b
MrHurried <69963194+MrHurried@users.noreply.github.com>
2023-02-12 09:14:54 +0100

    Worked some more on the streak text colors

--- Assets/Scripts/GameMode2/StreakScript.cs
+++ Assets/Scripts/GameMode2/StreakScript.cs
@@ -10,8 +10,16 @@ public class StreakScript : MonoBehaviour
     private Color isaTopColor, isaBottomColor;
     private Color matTopColor, matBottomColor;
 
+    private const float S_TopColor = 82/100;
+    private const float S_BottomColor = 62/100;
+    private const float V_TopColor = 58/100;
+    private const float V_BottomColor = 91/100;
+    //the Hue values at every reset
+    private const float H_TopDefault = 274/360;
+    private const float H_BottomDefault = 256/360;
+
     //Measured in degrees (max = 360; min = 0)
-    public int HueChangeMagnitude = 2;
+    public float HueChangeMagnitude = 2/360;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,29 +29,42 @@ public class StreakScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        updateTextColor();
+    }
+
+    void updateTextColor()
+    {
+        //streakOnderIsaText.colorGradient = new VertexGradient(isaTopColor, isaTopColor, isaBottomColor, isaBottomColor);
     }
 
     void resetGradient()
     {
-        matTopColor = Color.HSVToRGB(274, 82, 58);
-        matBottomColor = Color.HSVToRGB(256, 62, 91);
+        matTopColor = Color.HSVToRGB(H_TopDefault, S_TopColor, V_TopColor);
+        matBottomColor = Color.HSVToRGB(H_BottomDefault, S_BottomColor, V_BottomColor);
 
-        isaTopColor = Color.HSVToRGB(274, 82, 58);
-        isaBottomColor = Color.HSVToRGB(256, 62, 91);
+        isaTopColor = Color.HSVToRGB(H_TopDefault, S_TopColor, V_TopColor);
+        isaBottomColor = Color.HSVToRGB(H_BottomDefault, S_BottomColor, V_BottomColor);
 
         streakOnderIsaText.colorGradient = new VertexGradient(isaTopColor, isaTopColor, isaBottomColor, isaBottomColor);
     }
 
     void shiftHue(bool isMatisse)
     {
+        float H, S, V;
+
         if (isMatisse)
         {
 
         }
         else
         {
-            //if ((Color.RGBToHSV(isaBottomColor)).hue - HueChangeMagnitude < 0) isaBottomColor = Color.HSVToRGB(0, isa , );
+            Color.RGBToHSV(isaBottomColor, out H, out S, out V);
+            if ((H - HueChangeMagnitude < 0)) isaBottomColor = Color.HSVToRGB(0, S_BottomColor, V_BottomColor);
+            else
+            {
+                isaBottomColor = Color.HSVToRGB(H - HueChangeMagnitude, S_BottomColor, V_BottomColor);
+                isaTopColor = Color.HSVToRGB(H - HueChangeMagnitude, S_TopColor, V_TopColor);
+            }
             //streakOnderIsaText.color = new VertexGradient()
         }
     }
